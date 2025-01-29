0bf8a99041cc27d956fe1d6eaa2a77f014192679
lukeholmes <Luke.Holmes@uvm.edu>
2025-01-29 13:49:08 -0500

    add return and correct start mem address

--- CPU.py
+++ CPU.py
@@ -1,6 +1,3 @@
-from poetry.core.masonry.utils.include import Include
-from sympy.strategies.core import switch
-
 from Instruction import Instruction
 NUM_REGISTERS = 16
 MEM_SIZE = 65536
@@ -8,16 +5,16 @@ MEM_SIZE = 65536
 class CPU:
     def __init__(self):
         # Initialize program counter and next program counter
-        self.pc = 0
-        self.next_pc = 0
+        self.pc = 100
+        self.next_pc = 100
         # Initialize memory and registers with zeros
         self.memory = [0] * MEM_SIZE
         self.registers = [0] * NUM_REGISTERS
 
     def IF (self):
-        instruction = self.memory[self.pc]
+        instruction_word = self.memory[self.pc]
         self.next_pc += 1
-        return instruction
+        return instruction_word
 
     def ID (self, instruction_word):
         opcode = (instruction_word >> 28) & 0xF  # Bits 28-31
@@ -32,28 +29,31 @@ class CPU:
         match Instruction.opcode:
             case 1: #ADD
                 alu_result = Instruction.Rs1 + Instruction.Rs2
-                self.registers[Instruction.Rd] = alu_result
+                if Instruction.Rd != 0:
+                    self.registers[Instruction.Rd] = alu_result
             case 2: #ADDI
                 alu_result = Instruction.Rs1 + Instruction.immed
-                self.registers[Instruction.Rd] = alu_result
+                if Instruction.Rd != 0:
+                    self.registers[Instruction.Rd] = alu_result
             case 3: #BEQ
                 if Instruction.Rs1 == Instruction.Rs2:
                     self.next_pc = self.pc + Instruction.immed
             case 4: #JAL
                 alu_result = self.pc + 1
-                self.registers[Instruction.Rd] = alu_result
+                if Instruction.Rd != 0:
+                    self.registers[Instruction.Rd] = alu_result
                 self.next_pc = self.pc + Instruction.immed
             case 5: #LW
                 eff_address = Instruction.Rs1 + Instruction.immed
-                self.registers[Instruction.Rd] = self.memory[eff_address]
+                if Instruction.Rd != 0:
+                    self.registers[Instruction.Rd] = self.memory[eff_address]
             case 6: #SW
                 eff_address = Instruction.Rs2 + Instruction.immed
                 self.memory[eff_address] = Instruction.Rs1
+            case 7:
+                for i in range(NUM_REGISTERS):
+                    print("R" + str(i) + "=" + str(self.registers[i]) )
 
         self.pc = self.next_pc
-
-
-
-
 
 
